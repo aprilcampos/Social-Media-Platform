8f2b7230167a6f89a1c78206b240f4b5a38b2ed6
april <aprilcamille.campos@gmail.com>
2023-10-24 20:21:28 +0800

    created comments for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_062254) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_121752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -65,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_062254) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "comments", "posts"
 end
